33802cf4597fae4af8d9d1507a11eedaa14da22c
Emmanuel Blot <emmanuel.blot@free.fr>
2015-04-10 17:12:15 +0200

    Start rewriting the parser in a cleaner way

--- clangparser.py
+++ clangparser.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-import sublime, sublime_plugin
-
 import re
 import os
 import sys
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from pprint import pformat, pprint
 from subprocess import Popen, PIPE
 from tempfile import mkdtemp, mkstemp
@@ -17,113 +15,42 @@ def RX(rexp, count):
     return re.sub(r'\(\?P\<(\w+)\>', '(?P<\\g<1>%d>' % count, rexp)
 
 
-class DoxyFunc(object):
-    """A C function
-    """
-
-    BOOL_KIND_CRE = re.compile(r'\w+_(is|has)_\w+')
-
-    def __init__(self, name, line):
-        self.name = name
-        self.line = line
-        self.ret = 'void'
-        self.args = []
-
-    def set_arg(self, argtype, argname=None):
-        self.args.append((argtype, argname))
-
-    def set_return(self, rettype):
-        self.ret = rettype
-
-    def to_doxygen(self):
-        doc = []
-        doc.append("/**")
-        doc.append(" * %s" % self.name)
-        doc.append(" *")
-        for argtype, argname in self.args:
-            if argtype.endswith('*'):
-                argdir = argtype.startswith('const ') and 'in' or 'in,out'
-            else:
-                argdir = 'in'
-            doc.append(" * @param[%s] %s" % (argdir, argname))
-        if self.ret != 'void':
-            doc.append(" * @return %s" % self._get_default_return_doc())
-        doc.append(" */")
-        return '\n'.join(doc)
-
-    def _get_default_return_doc(self):
-        if self.BOOL_KIND_CRE.match(self.name) and \
-           self.ret in ('bool', 'unsigned int', 'int', '_Bool'):
-                return '@c true if condition matches or @c false otherwise'
-        if self.ret == 'int':
-            return '@c OK or a negative POSIX error code on error'
-        if self.ret.endswith('*'):
-            return 'an instance of %s' % self.ret.rstrip(' *')
-        return ''
-
-    def __str__(self):
-        return self.to_doxygen()
+def ALT(*res):
+    return r'(?:' + r'|'.join(res) + r')'
 
 
-class DoxyFile(object):
-    """Container for functions in a single source file
-    """
-
-    MAX_SEEK_LINE = 4
-
-    def __init__(self, name):
-        self._functions = {}
-        self.name = name
-
-    def add_function(self, doxyfunc, line):
-        # print("Add %s %s %d" % (self.name, doxyfunc.name, line))
-        self._functions[line] = doxyfunc
-
-    def get_at_line(self, line):
-        if line in self._functions:
-            return self._functions[line]
-        for l in sorted(self._functions):
-            # print (l, self._functions[l].name)
-            if l < line:
-                continue
-            if l > line + self.MAX_SEEK_LINE:
-                break
-            return self._functions[l]
-        return ''
-
-
-class DoxyClang(object):
+class ClangParser(object):
     """Parse clang-check AST dump to extract useful hints for Doxygen
     """
 
-    DEPTH_RE = r'^(?P<depth>(?:[| ]*)[|`]-)'
+    DEPTH_RE = r'^(?P<depth>(?:[| ]*)[|`]-)?'
     DEF_RE = r'(?P<stmt>[A-Za-z]+)\s(?P<ref>0x[0-9a-f]+)\s'
     BACKREF_RE = r'(?:(parent|prev)\s(?P<bref>0x[0-9a-f]+)\s)?'
     SCRATCH_RE = r'(?:<scratch space>:(?P<scratch>\d+:\d+))'
-    PATH_RE = r'(?P<path>/[\w/\.]+:\d+:\d+)'
+    NULL_RE = r'(?P<null><<<NULL>>>)'
+    FIELD_RE = r"'(?P<fld>\w*)'"
+    PATH_RE = r'(?P<path>/[\w/\.\-]+:\d+:\d+)'
     LINE_RE = r'(?:line:(?P<line>\d+:\d+))'
     COL_RE = r'(?:col:(?P<col>\d+))'
-    LOC1_RE = r'(?:' + r'|'.join((PATH_RE, LINE_RE, COL_RE)) + r')'
+    ISLOC_RE = r'<invalid sloc>'
+    LOC1_RE = ALT(PATH_RE, LINE_RE, COL_RE, ISLOC_RE)
     LOC_RE = RX(LOC1_RE, 1) + r'(,\s' + RX(LOC1_RE, 2) + r')?'
-    RANGE_RE = r'<(?:' + r'|'.join((SCRATCH_RE, LOC_RE)) + ')>\s'
-    XRANGE_RE = r'(?:' + RX(LOC1_RE, 3) + '\s)?'
-    RIGHT_RE = r'(?P<right>.*)$'
+    RANGE_RE = r'<' + ALT(SCRATCH_RE, LOC_RE) + r'>'
+    XRANGE_RE = r'(?:\s' + RX(LOC1_RE, 3) + ')?'
+    RIGHT_RE = r'(?:\s(?P<right>.*)|)$'
     ANSI_CRE = re.compile(r'\x1b[^m]*m')
-    LEFT_CRE = re.compile(DEPTH_RE + DEF_RE + BACKREF_RE + RANGE_RE +
-                          XRANGE_RE + RIGHT_RE)
+    LONGDEF_RE = BACKREF_RE + RANGE_RE + XRANGE_RE
+    FULDEF_RE = ALT(LONGDEF_RE, FIELD_RE)
+    LEFT_RE = ALT(DEF_RE + FULDEF_RE, NULL_RE)
+    LINE_CRE = re.compile(DEPTH_RE + LEFT_RE + RIGHT_RE)
     RANGE_CRE = re.compile(RANGE_RE)
-    SCRATCH_CRE = re.compile(SCRATCH_RE)
-    FUNC_MODS_RE = r'(?P<fmods>(?:implicit|used|referenced) )*'
-    FUNC_CRE = re.compile(FUNC_MODS_RE + r"(?P<fname>\w+)\s'(?P<fsig>[^']+)'")
-    PARMVAR_MODS_RE = r'(?P<pvmods>(?:used) )*'
-    PARMVAR_CRE = re.compile(PARMVAR_MODS_RE + 
-                             r"(?:(?P<pvname>\w+)\s)?'(?P<pvsig>[^']+)'")
 
     CMD_JSON_NAME = 'compile_commands.json'
 
     def __init__(self, debug=False):
         self._debug = debug
-        self._doxyfile = None
+        # self._doxyfile = None
+        self._files = {}
         self._clang_path = '/usr/local/bin/arm-elf32-minix-clang-check'
         self._build_path = '/Users/eblot/Sources/Neotion/ndk/sandboxes/t29/build/minix'
 
@@ -135,83 +62,19 @@ class DoxyClang(object):
             # get rid of ANSI color markers
             l = self.ANSI_CRE.sub('', l)
             # compute the depth of a statement
-            mo = self.LEFT_CRE.match(l)
+            mo = self.LINE_CRE.match(l)
             if not mo:
                 if self._debug:
-                    print(l, file=sys.stderr)
+                    print("NO MATCH", l, file=sys.stderr)
                 continue
-            mo.group('depth').replace('`', '|').count('|'), l
-            depth = len(mo.group('depth')) / 2
-            stmt = mo.group('stmt')
-            if stmt.endswith('Expr'):
-                continue
-            if stmt.endswith('Stmt'):
-                continue
-            if stmt.endswith('Operator'):
-                continue
-            yield n, l, mo, depth
-
-    def _parse(self, fp):
-        srcfiles = defaultdict(DoxyFile)
-        srcfile = None
-        func = None
-        # get_parm_var = False
-        for n, l, mo in self._get_next_line(fp):
-            right = mo.group('right')
-            stmt = mo.group('stmt')
-            if stmt.endswith('FunctionDecl'):
-                for x in reversed(range(3)):
-                    kp = 'path%d' % (x+1)
-                    kv = mo.group(kp)
-                    if kv:
-                        filename = kv.split(':')[0]
-                        if filename not in srcfiles:
-                            srcfiles[filename] = DoxyFile(filename)
-                        srcfile = srcfiles[filename]
-                        break
-            if stmt == 'FunctionDecl':
-                # ignore scratch definitions
-                func = None
-                smo = self.SCRATCH_CRE.match(right)
-                if smo:
-                    if self._debug:
-                        print("Ignore %s" % right, file=sys.stderr)
-                    continue
-                fmo = self.FUNC_CRE.match(right)
-                if not fmo:
-                    print("Error %s" % right, file=sys.stderr)
-                fname = fmo.group('fname')
-                line = 0
-                linedef = mo.group('line1')
-                if linedef:
-                    line = int(linedef.split(':')[0])
-                else:
-                    linedef = mo.group('path1')
-                    if linedef:
-                        line = int(linedef.split(':')[1])
-                if not line:
-                    continue
-                func = DoxyFunc(fname, line)
-                fret = fmo.group('fsig').split('(')[0].strip()
-                func.set_return(fret)
-                srcfile.add_function(func, line)
-                continue
-            if stmt == 'ParmVarDecl':
-                pvmo = self.PARMVAR_CRE.match(right)
-                if not pvmo:
-                    print(n, l, file=sys.stderr)
-                if not func:
-                    if self._debug:
-                        print("Orpheanous param @ %d: %s" % (n, l), 
-                              file=sys.stderr)
-                    continue
-                func.set_arg(pvmo.group('pvsig'), pvmo.group('pvname'))
-                #if fname.find('seqfs_') >= 0:
+            depthstr = mo.group('depth')
+            if depthstr:
+                depthstr.replace('`', '|').count('|'), l
+                depth = len(mo.group('depth')) // 2
             else:
-                # stop function argument retrieval
-                func = None
-            # if stmt == '':
-        return srcfiles
+                depth = 0
+            stmt = mo.group('stmt')
+            yield n, l, mo, depth
 
     def _clang_check(self, filename, cmddir):
         args = [self._clang_path, '--ast-dump', '-p', cmddir, filename]
@@ -286,45 +149,244 @@ class DoxyClang(object):
             return None
         return self._doxyfile.get_at_line(line)
 
+    @staticmethod
+    def get_clang_class(name):
+        try:
+            return getattr(sys.modules[__name__], 'Clang%s' % name)
+        except AttributeError:
+            return None
 
-class DoxyclangCommand(sublime_plugin.TextCommand):
+    def build_tree(self, fp):
+        stack = deque()
+        filename = ''
+        for n, l, m, d in self._get_next_line(fp):
+            filename = self._extract_filename(m, filename)
+            stmt = m.group('stmt')
+            cls = self.get_clang_class(stmt) or ClangDefaultObject
+            obj = cls(self, m, filename)
+            depth = len(stack)
+            move = d-depth
+            if not depth:
+                stack.append(obj)
+                continue
+            if move > 0:
+                if move > 1: 
+                    print("ERROR: too deep")
+                    break
+                stack[-1].add_child(obj)
+                stack.append(obj)
+                continue
+            while move < 0:
+                stack.pop()
+                move += 1
+            parent = stack[-1]
+            parent.add_child(obj)
+        print(stack)
+        stack[0].dump(0)
 
-    def _read_line(self, point):
-        if (point >= self.view.size()):
+    def register_function(self, clobj, filename):
+        if filename not in self._files:
+            self._files[filename] = FileContainer(filename)
+        self._files[filename].add_function(clobj)
+
+    @classmethod
+    def _extract_filename(cls, mo, default):
+        # Ugly heuristic, there should be a better way to find the exact
+        # path where a function belongs
+        for x in reversed(range(3)):
+            kp = 'path%d' % (x+1)
+            kv = mo.group(kp)
+            if kv:
+                return kv.split(':')[0]
+        return default
+
+
+class ClangObject(object):
+    """Base class
+    """
+
+    DUMP_INDENT = 4
+
+    def __init__(self, parser, mo, filename):
+        self._children = []
+        self.parser = parser
+        self.filename = filename
+
+    def add_child(self, child):
+        assert(isinstance(child, ClangObject))
+        self._children.append(child)
+
+    def dump(self, depth):
+        print(' ' * depth, self)
+        self._dump_children(depth + self.DUMP_INDENT)
+
+    def _dump_children(self, depth):
+        for c in self._children:
+            c.dump(depth)
+
+    def __str__(self):
+        return self.__class__.__name__
+
+
+class ClangDefaultObject(ClangObject):
+    """A clang object that is not parsed"""
+
+    def __init__(self, parser, mo, filename):
+        super(ClangDefaultObject, self).__init__(parser, mo, filename)
+        self.kind = mo.group('stmt')
+
+
+class ClangTranslationUnitDecl(ClangObject):
+    """Root object"""
+
+    def __init__(self, parser, mo, filename):
+        super(ClangTranslationUnitDecl, self).__init__(parser, mo, filename)
+
+
+class ClangFunctionDecl(ClangObject):
+    """Function declaration"""
+
+    SCRATCH_RE = r'(?:<scratch space>:(?P<scratch>\d+:\d+))'
+    SCRATCH_CRE = re.compile(SCRATCH_RE)
+    FUNC_MODS_RE = r'(?P<fmods>(?:implicit|used|referenced) )*'
+    FUNC_CRE = re.compile(FUNC_MODS_RE + r"(?P<fname>\w+)\s'(?P<fsig>[^']+)'")
+
+    def __init__(self, parser, mo, filename, debug=False):
+        super(ClangFunctionDecl, self).__init__(parser, mo, filename)
+        self.name = ''
+        self.line = 0
+        right = mo.group('right')
+        smo = self.SCRATCH_CRE.match(right)
+        if smo:
+            if debug:
+                print("Ignore %s" % right, file=sys.stderr)
             return
-        return self.view.substr(self.view.line(point))
-
-    def _get_document_text(self, point):
-        before_insert = self.view.substr(sublime.Region(0, point-3))
-        after_insert = self.view.substr(sublime.Region(point, self.view.size()))
-        return ''.join((before_insert, after_insert))
-
-    def run(self, edit, mode=None):
-        extension = self.view.window().extract_variables()['file_extension']
-        if extension not in ('c',):
-            return '\n'
-        point = self.view.sel()[0].begin()
-        linestr = self._read_line(point)
-        filename = self.view.file_name()
-        line, col = self.view.rowcol(point)
-        line += 1 # first line starts at 0
-        # print("%04d: %s" % (line, linestr))
-        dc = DoxyClang(False)
-        # dc.parse(filename)
-        buf = self._get_document_text(point)
-        #for n, x in enumerate(buf.split('\n'), 1):
-        #    print ("%04d: %s" % (n, x))
-        #return
-        dc.parse_buffer(filename, buf)
-        func = dc.get_func(line)
-        if func:
-            doc = str(func)[len(linestr):]
-            self.view.insert(edit, point, doc)
+        fmo = self.FUNC_CRE.match(right)
+        if not fmo:
+            print("Error %s" % right, file=sys.stderr)
+        fname = fmo.group('fname')
+        line = 0
+        linedef = mo.group('line1')
+        if linedef:
+            line = int(linedef.split(':')[0])
         else:
-            self.view.insert(edit, point, '\n *\n */')
+            linedef = mo.group('path1')
+            if linedef:
+                line = int(linedef.split(':')[1])
+        if not line:
+            return
+        self.name = fname
+        self.line = line
+        self.ret = fmo.group('fsig').split('(')[0].strip()
+        self.parser.register_function(self, filename)
+        #print(fname, line)
+        #func = DoxyFunc(fname, line)
+        #func.set_return(fret)
+        #srcfile.add_function(func, line)
+                
+    def __str__(self):
+        return '%s: %s @ %s:%d' % (self.__class__.__name__, 
+                                   self.name, os.path.basename(self.filename),
+                                   self.line)
+
+
+class ClangParmVarDecl(ClangObject):
+    """Function parameter variable"""
+
+    PARMVAR_MODS_RE = r'(?P<pvmods>(?:used) )*'
+    PARMVAR_CRE = re.compile(PARMVAR_MODS_RE + 
+                             r"(?:(?P<pvname>\w+)\s)?'(?P<pvsig>[^']+)'")
+
+    def __init__(self, parser, mo, filename):
+        super(ClangParmVarDecl, self).__init__(parser, mo, filename)
+        pvmo = self.PARMVAR_CRE.match(mo.group('right'))
+        if not pvmo:
+            print(n, l, file=sys.stderr)
+            self.name = ''
+            self.signature = ''
+        else:
+            self.name = pvmo.group('pvname')
+            self.signature = pvmo.group('pvsig')
+
+    def __str__(self):
+        return '%s: %s %s' % (self.__class__.__name__, 
+                              self.signature, self.name)
+
+
+class Function(object):
+    """A C function
+    """
+
+    BOOL_KIND_CRE = re.compile(r'\w+_(is|has)_\w+')
+
+    def __init__(self, name, line):
+        self.name = name
+        self.line = line
+        self.ret = 'void'
+        self.args = []
+
+    def set_arg(self, argtype, argname=None):
+        self.args.append((argtype, argname))
+
+    def set_return(self, rettype):
+        self.ret = rettype
+
+    def to_doxygen(self):
+        doc = []
+        doc.append("/**")
+        doc.append(" * %s" % self.name)
+        doc.append(" *")
+        for argtype, argname in self.args:
+            if argtype.endswith('*'):
+                argdir = argtype.startswith('const ') and 'in' or 'in,out'
+            else:
+                argdir = 'in'
+            doc.append(" * @param[%s] %s" % (argdir, argname))
+        if self.ret != 'void':
+            doc.append(" * @return %s" % self._get_default_return_doc())
+        doc.append(" */")
+        return '\n'.join(doc)
+
+    def _get_default_return_doc(self):
+        if self.BOOL_KIND_CRE.match(self.name) and \
+           self.ret in ('bool', 'unsigned int', 'int', '_Bool'):
+                return '@c true if condition matches or @c false otherwise'
+        if self.ret == 'int':
+            return '@c OK or a negative POSIX error code on error'
+        if self.ret.endswith('*'):
+            return 'an instance of %s' % self.ret.rstrip(' *')
+        return ''
+
+    def __str__(self):
+        return self.to_doxygen()
+
+
+class FileContainer(object):
+    """Container for functions in a single source file
+    """
+
+    MAX_SEEK_LINE = 4
+
+    def __init__(self, name):
+        self._functions = {}
+        self.name = name
+
+    def add_function(self, clfunc):
+        self._functions[clfunc.line] = clfunc
+
+    def get_at_line(self, line):
+        if line in self._functions:
+            return self._functions[line]
+        for l in sorted(self._functions):
+            # print (l, self._functions[l].name)
+            if l < line:
+                continue
+            if l > line + self.MAX_SEEK_LINE:
+                break
+            return self._functions[l]
+        return ''
 
 
 if __name__ == '__main__':
-    dc = DoxyClang(False)
-    for n, l, m, d in dc._get_next_line(sys.stdin): 
-        print (n, d, l)
+    cp = ClangParser(True)
+    cp.build_tree(sys.stdin.buffer)
